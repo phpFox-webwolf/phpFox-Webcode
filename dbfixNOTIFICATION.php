2c0885f84b5f06ac624182b9d55ed30288a5f941
phpFox-webwolf <webwolf@insightbb.com>
2013-08-18 18:30:03 -0400

    Tool for NewUserPreset module for phpfox
    This replaces the user_notification table data with settings from the userpreset module.

--- dbfixNOTIFICATION.php
+++ dbfixNOTIFICATION.php
@@ -2,40 +2,7 @@
  
 /**
  * [PHPFOX_HEADER]
- */
-
-/**
  * 
- * 
- * @copyright  Open Source
- * @author     Steward/Webwolf
- * @package    User Privacy Notification Presets/tools
- * @version    v1.0
- */
- 
-/* Make sure we are running PHP5
-*/
-if (version_compare(phpversion(), '5', '<') === true)
-{
-exit('phpFox 2 or higher requires PHP 5 or newer.');
-}
- 
-ob_start();
- 
-/**
-* Key to include phpFox
-*
-*/
-define('PHPFOX', true);
- 
-/**
-* Directory Seperator
-*
-*/
-define('PHPFOX_DS', DIRECTORY_SEPARATOR);
- 
-/**
-* phpFox Root Directory
 *
 */
 define('PHPFOX_DIR', dirname(dirname(__FILE__)) . PHPFOX_DS);
