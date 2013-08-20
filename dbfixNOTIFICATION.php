7bf3802daa2e787b8b04f6dff647a61dace1a20e
phpFox-webwolf <webwolf@insightbb.com>
2013-08-19 22:54:47 -0400

    Update dbfixNOTIFICATION.php

--- dbfixNOTIFICATION.php
+++ dbfixNOTIFICATION.php
@@ -15,6 +15,31 @@ You are free to modify and use for your purposes but please leave this credit in
 * @version v1.0
 */
  
+/* Make sure we are running PHP5
+*/
+if (version_compare(phpversion(), '5', '<') === true)
+{
+exit('phpFox 2 or higher requires PHP 5 or newer.');
+}
+ 
+ob_start();
+ 
+/**
+* Key to include phpFox
+*
+*/
+define('PHPFOX', true);
+ 
+/**
+* Directory Seperator
+*
+*/
+define('PHPFOX_DS', DIRECTORY_SEPARATOR);
+ 
+/**
+* phpFox Root Directory
+*
+*/
 define('PHPFOX_DIR', dirname(dirname(__FILE__)) . PHPFOX_DS);
  
 define('PHPFOX_START_TIME', array_sum(explode(' ', microtime())));
